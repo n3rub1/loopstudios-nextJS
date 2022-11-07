64d06b2ecc5e4aa1ba7f96eb8555e266788089f1
Nerubi <sm4391@stmartins.edu>
2022-11-07 07:39:13 +0100

    added responsiveness to middle hero

--- pages/components/Menu.tsx
+++ pages/components/Menu.tsx
@@ -1,12 +1,14 @@
 import close from "../../public/icon-close.svg";
 import Image from "next/image";
 
-export function Menu() {
+export function Menu(props:any) {
+
+console.log(props)
+
     return (
         <section className="menu-mobile">
             <div className="top-menu-mobile">
-                {/* <h2>loopstudios</h2> */}
-                <Image src={close} alt="close"></Image>
+                <Image onClick={()=>{props.stateChange(false)}} src={close} alt="close"></Image>
             </div>
             <div className="bottom-menu-mobile">
                 <ul className="menu-items-mobile">
--- pages/components/topnav.tsx
+++ pages/components/topnav.tsx
@@ -3,42 +3,45 @@ import Image from "next/image";
 import { Menu } from "../components/Menu";
 import burger from "../../public/icon-hamburger.svg";
 
-
 export function TopNav() {
+  const [burgerClicked, setBurgerClicked] = React.useState(false);
 
-    const [burgerClicked, setBurgerClicked] = React.useState(false);
-
-    return (
-        <header className="top-part">
-            <nav className="top-nav">
-                <ul className="top-nav-list">
-                    <li className="companyName">
-                        <a href="#">loopstudios</a>
-                    </li>
-                </ul>
-                {burgerClicked && <Menu></Menu>}
-                {!burgerClicked && <Image src={burger} className="burger" alt="burger"></Image>}
-                <ul className="top-nav-list">
-                    <li className="side-nav">
-                        <a href="about">About</a>
-                    </li>
-                    <li className="side-nav">
-                        <a href="news">Careers</a>
-                    </li>
-                    <li className="side-nav">
-                        <a href="events">Events</a>
-                    </li>
-                    <li className="side-nav">
-                        <a href="products">Products</a>
-                    </li>
-                    <li className="side-nav">
-                        <a href="support">Support</a>
-                    </li>
-                </ul>
-            </nav>
-            <h1 className="hero-text">IMMERSIVE EXPERIENCES THAT DELIVER</h1>
-        </header>
-    )
+  const invertBurger = ()=>{
+    setBurgerClicked(prev => !prev)
+  }
 
 
+  return (
+    <header className="top-part">
+      <nav className="top-nav">
+        <ul className="top-nav-list">
+          <li className="companyName">
+            <a href="#">loopstudios</a>
+          </li>
+        </ul>
+        {burgerClicked && <Menu
+        stateChange={setBurgerClicked}
+        ></Menu>}
+        <ul className="top-nav-list">
+          {!burgerClicked && <div onClick={invertBurger} className="burger">🍔</div>}
+          <li className="side-nav">
+            <a href="about">About</a>
+          </li>
+          <li className="side-nav">
+            <a href="news">Careers</a>
+          </li>
+          <li className="side-nav">
+            <a href="events">Events</a>
+          </li>
+          <li className="side-nav">
+            <a href="products">Products</a>
+          </li>
+          <li className="side-nav">
+            <a href="support">Support</a>
+          </li>
+        </ul>
+      </nav>
+      <h1 className="hero-text">IMMERSIVE EXPERIENCES THAT DELIVER</h1>
+    </header>
+  );
 }
